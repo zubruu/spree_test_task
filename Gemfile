4c4326859054b34ce59a5e35dc525e2b6c425ad6
Michał Zubrzycki <zubruu@gmail.com>
2018-09-26 21:47:04 +0200

    Added delayed_jobs and errors
    Delayed_jobs for importing in parts
    Simple errors handling

--- Gemfile
+++ Gemfile
@@ -35,6 +35,9 @@ gem 'spree', '~> 3.4.0'
 gem 'spree_auth_devise', '~> 3.3'
 gem 'spree_gateway', '~> 3.3'
 
+gem 'delayed_job_active_record'
+gem 'daemons'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/models/import.rb
+++ app/models/import.rb
@@ -1,33 +1,43 @@
 require 'csv'
 
 class Import
-
-  TAXONS_AVAIABLE_PARAMS = %w(category category_id)
-  STOCK_AVAIABLE_PARAMS = %w(stock_total)
-  EXCEPT_PARAMS = TAXONS_AVAIABLE_PARAMS + STOCK_AVAIABLE_PARAMS
-
-  PRODUCT_REPLACE_PARAMS_NAME = {'availability_date' => 'available_on'}
-  VARIANTS_OPTIONS_PREFIX = 'option_'
-
+  BATCH_SIZE = 2
 
   def initialize(file_path, import_type)
     @file_path = file_path
     @import_model = import_type
+    @file_length = CSV.read(file_path).length
   end
 
-  def import
+  def start_import
     set_default
-    CSV.foreach('sample.csv', {headers: true, col_sep: ';'}) do |row|
+    Import.import(@file_path, @import_model, 0, @file_length)
+  end
+
+  def self.import(file_path, import_model, drop_count = 0, file_length)
+    i = 0
+    CSV.foreach('sample.csv', {headers: true, col_sep: ';'}).drop(drop_count).each do |row|
       next unless row.to_h.compact.present?
-      import_row = @import_model.new(row.to_h.compact)
-      import_row.do_import
+      begin
+        import_row = import_model.new(row.to_h.compact)
+        import_row.do_import
+      rescue StandardError => e
+        log_error(e, row)
+      end
+      i = i +1
+      break if drop_count + BATCH_SIZE == i
     end
+    Import.delay.import(file_path, import_model, drop_count + BATCH_SIZE, file_length) if drop_count + BATCH_SIZE < file_length
   end
 
   private
 
   def set_default
     Spree::StockLocation.create!(name: 'default') if Spree::StockLocation.all.blank?
+  end
+
+  def self.log_error(error, row)
+    Rails.logger.error("Error importing: row: #{row}, error: #{error}")
   end
 
   def self.clean
